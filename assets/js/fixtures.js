ae7047976035fe92e29ee094137734d2d701242e
rbrd87 <rochellembradley@gmail.com>
2024-03-22 10:48:35 +0000

    Reworked the API calls to handle errors, changed the headers for the sub pages

--- assets/js/fixtures.js
+++ assets/js/fixtures.js
@@ -1,18 +1,33 @@
-function getFixRes() {
-    // 3rd API for generating cocktail image
-    var fixResUrl = "https://gmsfeed.co.uk/api/show/refresh?method=api&show=results%2Bfixtures&team=261f3848-3f43-4ab1-98b4-c40709bd4731&comp_id=c0c15e15-bfdd-48b1-9568-5387b1742a1a&sort_by=fixtureTime"
+function errorMessage() {
+    $("#fixtures-list").append(`<div class="alert alert-danger" role="alert">
+    There has been an error - please try again later.
+  </div>`)
+}
 
+function getFixtures() {
+    // ajax call to get the fixtures data from GMS Feed
     $.ajax({
-        url: fixResUrl,
-        method: "GET"
-    }).then(function (fixResResponse) {
-
-        $("#fixtures-list").append(`${fixResResponse.html}`)
+      method: 'GET',
+      url: 'https://gmsfeed.co.uk/api/show/refresh?method=api&show=results%2Bfixtures&team=261f3848-3f43-4ab1-98b4-c40709bd4731&comp_id=c0c15e15-bfdd-48b1-9568-5387b1742a1a&sort_by=fixtureTime',
+      contentType: 'application/json',
+      success: function (fixtureData) {
+        // If the call is succesful the following will be executed
+        console.log(fixtureData);
+  
+        // Appended to the fixtures-list on the fixtures page
+        $("#fixtures-list").append(`${fixtureData.html}`);
+      },
+      error: function ajaxError(errorData) {
+        // If the call errors the following will be executed
+        console.error('Error: ', errorData.responseText);
+  
+        errorMessage();
+      }
     });
-};
+  };
 
-$(document).ready(function () {
+  $(document).ready(function () {
 
-    getFixRes();
+    getFixtures();
 
 });
--- assets/js/table.js
+++ assets/js/table.js
@@ -1,18 +1,33 @@
-function getFixRes() {
-    // 3rd API for generating cocktail image
-    var tableUrl = "https://gmsfeed.co.uk/api/show/refresh?method=api&show=league&team=261f3848-3f43-4ab1-98b4-c40709bd4731&comp_id=c0c15e15-bfdd-48b1-9568-5387b1742a1a&sort_by=fixtureTime&options=showDetail:yes"
+function errorMessage() {
+    $("#tableElement").append(`<div class="alert alert-danger" role="alert">
+    There has been an error - please try again later.
+  </div>`)
+}
 
+function getTable() {
+    // ajax call to get the table data from GMS Feed
     $.ajax({
-        url: tableUrl,
-        method: "GET"
-    }).then(function (table) {
-        
-        $("#tableElement").append(`${table.html}`)
+      method: 'GET',
+      url: 'https://gmsfeed.co.uk/api/show/refresh?method=api&show=league&team=261f3848-3f43-4ab1-98b4-c40709bd4731&comp_id=c0c15e15-bfdd-48b1-9568-5387b1742a1a&sort_by=fixtureTime&options=showDetail:yes',
+      contentType: 'application/json',
+      success: function (tableData) {
+        // If the call is succesful the following will be executed
+        console.log(tableData);
+  
+        // Appended to the tableElement on the table page
+        $("#tableElement").append(`${tableData.html}`);
+      },
+      error: function ajaxError(errorData) {
+        // If the call errors the following will be executed
+        console.error('Error: ', errorData.responseText);
+  
+        errorMessage();
+      }
     });
-};
+  };
 
-$(document).ready(function () {
+  $(document).ready(function () {
 
-    getFixRes();
+    getTable();
 
 });
